d361100ff8d506bfcbe3d7957c7c7456ab707837
Sahil Lavingia <sahil@gumroad.com>
2017-02-28 20:57:02 -0700

    simple white screen for loading (#69)

--- frontend/components/Loading.jsx
+++ frontend/components/Loading.jsx
@@ -4,7 +4,7 @@ export default class Loading extends Component {
     render() {
         return (
             <div className="loadingIndicator">
-                Loading...
+
             </div>
         )
     }
